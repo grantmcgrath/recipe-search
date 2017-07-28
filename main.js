d45cf411b6a190cd820a8ff4f6f5115696b2b747
Grant McGrath <grantmcgrath@me.com>
2017-07-28 17:38:36 -0400

    Completed functionality. Going to continue with design. Completed the Minimum Viable Product.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var button = document.querySelector("#searchButton");
 var request = document.querySelector("#input");
-let results = document.querySelector("#results")
+let retResults = document.querySelector("#results")
 
-console.log(request.value);
+// console.log(request.value);
 
 button.addEventListener("click", function() {
+  retResults.innerHTML = ` `;
   fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${request.value}`)
     .then(function(response) {
       if (response.status !== 200) {
@@ -12,19 +13,14 @@ button.addEventListener("click", function() {
         return;
       }
       response.json().then(function(data) {
-        console.log("Here is the data:", data);
-      })
-    })
-
-    // HTML that is injected with the search results
-    .then(function(data) {
       let results = data.results;
-      console.log(result);
+      // console.log(results);
+      // console.log(results.length);
       for (var i = 0; i < results.length; i++) {
-        results.innerHTML += `
-      <div class="">
-        <a href=${results[i].href}>
-          <img src="${results[i].thumbnail}" alt="This is a " />
+        retResults.innerHTML += `
+      <div class="boxedResults">
+        <a href="${results[i].href}">
+          <img src="${results[i].thumbnail}" alt="No image available of ${results[i].title}" target="_blank">
           <p>${results[i].title}</p>
         </a>
       </div>
@@ -34,4 +30,4 @@ button.addEventListener("click", function() {
     .catch(function(error) {
       console.log("Fetch Error: ", error);
     })
-})
+})})
