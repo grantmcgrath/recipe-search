7fe84eebe37db0b0b71aee93e38a5c20d830c3eb
Grant McGrath <grantmcgrath@me.com>
2017-07-28 15:50:36 -0400

    Finally! Got the search to work. Still need to get the results printed to screen correctly.

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-fetch("http://www.recipepuppy.com/api/")
-.then(
+var button = document.querySelector("#searchButton");
+var request = document.querySelector("#input");
+let results = document.querySelector("#results")
 
-  function (response) {
-    if (response.status !== 200){
-      console.log(response.status);
-      return;
-    }
-    response.json().then(function(data){
-      console.log("Here is the data:", data);
+console.log(request.value);
+
+button.addEventListener("click", function() {
+  fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${request.value}`)
+    .then(function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        console.log("Here is the data:", data);
+      })
     })
-  }
-)
 
-.catch(function(err){
-  console.log("Fetch Error :-s" , err);
-});
+    // HTML that is injected with the search results
+    .then(function(data) {
+      let results = data.results;
+      console.log(result);
+      for (var i = 0; i < results.length; i++) {
+        results.innerHTML += `
+      <div class="">
+        <a href=${results[i].href}>
+          <img src="${results[i].thumbnail}" alt="This is a " />
+          <p>${results[i].title}</p>
+        </a>
+      </div>
+      `
+      }
+    })
+    .catch(function(error) {
+      console.log("Fetch Error: ", error);
+    })
+})
